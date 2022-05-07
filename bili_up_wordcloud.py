71f49e409aaa2f091d6983f0739d8a9a9ea7f60b
zack <void_zack@qq.com>
2022-05-07 13:17:46 +0800

    commit v0.2

---
+++ bili_up_wordcloud.py
@@ -0,0 +1,10 @@
+import wordcloud
+import pandas as pd
+UID = '1339327684'
+tags = pd.read_csv(f'UID-{UID}.csv', header=None)
+tags.columns=['tag']
+wtags = tags.groupby('tag').size()
+wc= wordcloud.WordCloud(font_path="NotoSerifCJK-Bold.ttc",
+                        background_color="white",
+                        width =1920,height= 1080,margin= 10 ).generate_from_frequencies(wtags)
+wc.to_file(f'UID-{UID}-词云.png')
